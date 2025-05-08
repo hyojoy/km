f412d6d53dd71f53ccb162e030b7a61c13784eea
hyojoy <80305266+hyojoy@users.noreply.github.com>
2025-05-08 23:00:37 +0900

    Update kms.py

--- kms.py
+++ kms.py
@@ -12,7 +12,8 @@ options = Options()
 options.add_argument("--headless")
 options.add_argument("--disable-gpu")
 options.add_argument("--window-size=1200x800")
-
+options.add_argument("--no-sandbox") 
+options.add_argument("--disable-dev-shm-usage") 
 driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
 
 # 서비스 목록 및 키워드 RAW 데이터
